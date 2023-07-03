df62cd07c4015ad5291ae497e79f43d24a10dc3e
Rob Lund <rob.lund@onxmaps.com>
2023-07-03 16:39:07 -0600

    Trying the null_ls builtin formatters again

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,47 +1,26 @@
 return {
     "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {
-        { "MunifTanjim/prettier.nvim" },
-    },
     config = function()
         local null_ls = require("null-ls")
-        local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
         null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.lua_format,
+            },
             on_attach = function(client, bufnr)
-                if client.server_capabilities.documentFormattingProvider then
-                    -- format on save
-                    vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
                     vim.api.nvim_create_autocmd("BufWritePre", {
-                            buffer = bufnr,
-                            group = group,
-                            callback = function()
-                                vim.lsp.buf.format({ bufnr = bufnr, async = false })
-                            end,
-                            desc = "[lsp] format on save",
-                        })
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
                 end
             end,
         })
-
-        local prettier = require('prettier')
-        prettier.setup {
-          bin = 'prettier',
-          filetypes = {
-              "css",
-              "graphql",
-              "html",
-              "javascript",
-              "javascriptreact",
-              "json",
-              "less",
-              "markdown",
-              "scss",
-              "typescript",
-              "typescriptreact",
-              "yaml",
-              "vue",
-          }
-        }
     end
 }
