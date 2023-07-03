4d3a1465b093925bb47b97feb94d9749f5a81e24
Rob Lund <rob.lund@onxmaps.com>
2023-07-03 09:01:08 -0600

    Try out neotest

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,32 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-neotest/neotest-jest",
+    },
+    config = function ()
+        local neotest = require("neotest")
+        neotest.setup({
+            adapters = {
+                require("neotest-jest"),
+            },
+            icons = {
+                passed = "P",
+                failed = "F",
+                running = ">",
+                skipped = "#",
+            },
+        })
+        vim.keymap.set("n", "<leader>0", function()
+            require("neotest").run.run()
+        end, { desc = "Test nearest test" })
+        vim.keymap.set("n", "<leader>9", function ()
+            require("neotest").run.run(vim.fn.expand("%"))
+        end, { desc = "Test this file" })
+        vim.keymap.set("n", "<leader>8", function ()
+            require("neotest").output.open({ enter = true })
+        end, { desc = "Open the test output" })
+    end
+}
