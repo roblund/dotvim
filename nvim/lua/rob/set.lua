632c73da16c40f814c7b6f184668d551899f756c
Rob Lund <rob.lund@onxmaps.com>
2023-07-03 09:05:34 -0600

    Try autoindent over smartindent for now

--- nvim/lua/rob/set.lua
+++ nvim/lua/rob/set.lua
@@ -5,7 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.scrolloff = 3
-vim.opt.smartindent = true
+vim.opt.autoindent = true
 vim.opt.splitright = true
 
 vim.opt.colorcolumn = "80"
