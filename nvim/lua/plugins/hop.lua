27ccdc7214129d8308ca9e6df53c74123d2866e0
Rob Lund <rob.lund@onxmaps.com>
2025-02-17 14:57:51 -0700

    Add some additional hop mappings

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -7,5 +7,9 @@ return {
         require('hop').setup()
         vim.keymap.set("n", "s", vim.cmd.HopWord)
         vim.keymap.set("v", "s", vim.cmd.HopWord)
+        vim.keymap.set("n", "S", vim.cmd.HopChar1)
+        vim.keymap.set("v", "S", vim.cmd.HopChar1)
+        vim.keymap.set("n", "<leader>s", vim.cmd.HopLineStart)
+        vim.keymap.set("v", "<leader>s", vim.cmd.HopLineStart)
     end
 }
