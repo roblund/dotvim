212532d8e3c7f996382ef37dbe118533eba61cfc
Rob Lund <rob.lund@onxmaps.com>
2025-06-03 09:29:58 -0600

    Set default codecompanion model to claude 4

--- nvim/lua/plugins/companion.lua
+++ nvim/lua/plugins/companion.lua
@@ -4,5 +4,21 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
+		"ravitemer/codecompanion-history.nvim",
 	},
+	config = function()
+		require("codecompanion").setup({
+			adapters = {
+				copilot = function()
+					return require("codecompanion.adapters").extend("copilot", {
+						schema = {
+							model = {
+								default = "claude-sonnet-4",
+							},
+						},
+					})
+				end,
+			},
+		})
+	end,
 }
