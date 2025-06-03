571fb2ca13cb1df211404a99cc65c92632b25588
Rob Lund <rob.lund@onxmaps.com>
2025-06-03 09:29:16 -0600

    Fix colorscheme layout

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,35 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 999,
-        opts = {},
-        config = function()
-            require("tokyonight").setup({
-                style = "night",
-                on_colors = function(c)
-                    c.bg = "#000000"
-                    c.comment = "#6F90C2"
-                end,
-                on_highlights = function(hl, c)
-                    hl.LineNr = {
-                        fg = "#505C6D",
-                    }
-                    hl.TabLine = {
-                        fg = c.cyan
-                    }
-                    hl.StatusLine = {
-                        fg = "#d5d6db",
-                        bg = c.blue7,
-                    }
-                    hl.StatusLineNC = {
-                        fg = c.green1
-                    }
-                end
-            })
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 999,
+		opts = {},
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				on_colors = function(c)
+					c.bg = "#000000"
+					c.comment = "#6F90C2"
+				end,
+				on_highlights = function(hl, c)
+					hl.LineNr = {
+						fg = "#505C6D",
+					}
+					hl.TabLine = {
+						fg = c.cyan,
+					}
+					hl.StatusLine = {
+						fg = "#d5d6db",
+						bg = c.blue7,
+					}
+					hl.StatusLineNC = {
+						fg = c.green1,
+					}
+				end,
+			})
 
-            vim.opt.background = "dark"
-            vim.cmd([[colorscheme tokyonight-night]])
-        end
-    },
+			vim.opt.background = "dark"
+			vim.cmd([[colorscheme tokyonight-night]])
+		end,
+	},
 }
