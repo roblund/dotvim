cd739c5f53428959cdde982d78fb8b09dd31ca36
Rob Lund <rob.lund@onxmaps.com>
2025-06-03 10:37:48 -0600

    Added companion history and keybindings

--- nvim/lua/plugins/companion.lua
+++ nvim/lua/plugins/companion.lua
@@ -1,24 +1,41 @@
 return {
-	"olimorris/codecompanion.nvim",
-	opts = {},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"ravitemer/codecompanion-history.nvim",
-	},
-	config = function()
-		require("codecompanion").setup({
-			adapters = {
-				copilot = function()
-					return require("codecompanion.adapters").extend("copilot", {
-						schema = {
-							model = {
-								default = "claude-sonnet-4",
-							},
-						},
-					})
-				end,
-			},
-		})
-	end,
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "ravitemer/codecompanion-history.nvim",
+    },
+    config = function()
+        require("codecompanion").setup({
+            adapters = {
+                copilot = function()
+                    return require("codecompanion.adapters").extend("copilot", {
+                        schema = {
+                            model = {
+                                default = "claude-sonnet-4",
+                            },
+                        },
+                    })
+                end,
+            },
+            extensions = {
+                history = {
+                    enabled = true,
+                },
+            },
+        })
+
+        vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+        vim.keymap.set(
+            { "n", "v" },
+            "<Leader>a",
+            "<cmd>CodeCompanionChat Toggle<cr>",
+            { noremap = true, silent = true }
+        )
+        vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+        -- Expand 'cc ' into 'CodeCompanion' in the command line
+        vim.cmd([[cab cc CodeCompanion]])
+    end,
 }
