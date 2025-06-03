55bd38ced8888b4c70b6a5f4f3f2fa217423617e
Rob Lund <rob.lund@onxmaps.com>
2025-06-03 09:27:21 -0600

    Reorganize the treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,43 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
-    },
-    config = function()
-        require 'nvim-treesitter.configs'.setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "javascript", "typescript", "lua", "vim", "vimdoc", "query", "vue" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"javascript",
+					"typescript",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"vue",
+					"markdown",
+					"markdown_inline",
+				},
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+				sync_install = false,
+				auto_install = true,
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
 
-            highlight = {
-                enable = true,
+				indent = {
+					enable = true,
+				},
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end,
-    { "nvim-treesitter/playground" },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+		end,
+	},
 }
