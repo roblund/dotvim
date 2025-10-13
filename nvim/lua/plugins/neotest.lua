5009b90d3e81c0b916495109ae6fe3911a8addd8
Rob Lund <rob.lund@onxmaps.com>
2025-10-13 15:46:49 -0600

    Setting up neotest with vitest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,37 +1,39 @@
 return {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-neotest/neotest-jest",
-    },
-    config = function()
-        local neotest = require("neotest")
-        neotest.setup({
-            adapters = {
-                require("neotest-jest"),
-            },
-            icons = {
-                passed = "P",
-                failed = "F",
-                running = ">",
-                skipped = "#",
-                unknown = "?",
-            },
-            quickfix = {
-                open = false
-            },
-        })
-        vim.keymap.set("n", "<leader>0", function()
-            require("neotest").run.run()
-        end, { desc = "Test nearest test" })
-        vim.keymap.set("n", "<leader>9", function()
-            require("neotest").run.run(vim.fn.expand("%"))
-        end, { desc = "Test this file" })
-        vim.keymap.set("n", "<leader>8", function()
-            require("neotest").output.open({ enter = false })
-        end, { desc = "Open the test output" })
-    end
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-neotest/neotest-jest",
+		"marilari88/neotest-vitest",
+	},
+	config = function()
+		local neotest = require("neotest")
+		neotest.setup({
+			adapters = {
+				require("neotest-jest"),
+				require("neotest-vitest"),
+			},
+			icons = {
+				passed = "P",
+				failed = "F",
+				running = ">",
+				skipped = "#",
+				unknown = "?",
+			},
+			quickfix = {
+				open = false,
+			},
+		})
+		vim.keymap.set("n", "<leader>0", function()
+			require("neotest").run.run()
+		end, { desc = "Test nearest test" })
+		vim.keymap.set("n", "<leader>9", function()
+			require("neotest").run.run(vim.fn.expand("%"))
+		end, { desc = "Test this file" })
+		vim.keymap.set("n", "<leader>8", function()
+			require("neotest").output.open({ enter = false })
+		end, { desc = "Open the test output" })
+	end,
 }
