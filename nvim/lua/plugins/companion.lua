807060980b78d1f10464cb139244035e20c310d8
Rob Lund <rob.lund@onxmaps.com>
2025-10-13 15:46:20 -0600

    Add codecompanion

--- nvim/lua/plugins/companion.lua
+++ nvim/lua/plugins/companion.lua
@@ -1,51 +1,58 @@
 return {
-    "olimorris/codecompanion.nvim",
-    opts = {},
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "ravitemer/codecompanion-history.nvim",
-    },
-    config = function()
-        require("codecompanion").setup({
-            adapters = {
-                copilot = function()
-                    return require("codecompanion.adapters").extend("copilot", {
-                        schema = {
-                            model = {
-                                default = "claude-sonnet-4",
-                            },
-                        },
-                    })
-                end,
-            },
-            extensions = {
-                history = {
-                    enabled = true,
-                },
-            },
-        })
+	"olimorris/codecompanion.nvim",
+	opts = {},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"ravitemer/codecompanion-history.nvim",
+	},
+	config = function()
+		require("codecompanion").setup({
+			adapters = {
+				copilot = function()
+					return require("codecompanion.adapters").extend("copilot", {
+						schema = {
+							model = {
+								default = "claude-sonnet-4",
+							},
+						},
+					})
+				end,
+			},
+			extensions = {
+				history = {
+					enabled = true,
+				},
+			},
+			display = {
+				chat = {
+					window = {
+						width = "auto",
+					},
+				},
+			},
+		})
 
-        vim.keymap.set(
-            { "n", "v" },
-            "<C-a>",
-            "<cmd>CodeCompanionActions<cr>",
-            { noremap = true, silent = true, desc = "Open CodeCompanion Actions Palette" }
-        )
-        vim.keymap.set(
-            { "n", "v" },
-            "<Leader>a",
-            "<cmd>CodeCompanionChat Toggle<cr>",
-            { noremap = true, silent = true, desc = "Toggle CodeCompanion Chat" }
-        )
-        vim.keymap.set(
-            "v",
-            "ga",
-            "<cmd>CodeCompanionChat Add<cr>",
-            { noremap = true, silent = true, desc = "Add current selection to CodeCompanion Chat" }
-        )
+		vim.keymap.set(
+			{ "n", "v" },
+			"<C-a>",
+			"<cmd>CodeCompanionActions<cr>",
+			{ noremap = true, silent = true, desc = "Open CodeCompanion Actions Palette" }
+		)
+		vim.keymap.set(
+			{ "n", "v" },
+			"<Leader>a",
+			"<cmd>CodeCompanionChat Toggle<cr>",
+			{ noremap = true, silent = true, desc = "Toggle CodeCompanion Chat" }
+		)
+		vim.keymap.set(
+			"v",
+			"ga",
+			"<cmd>CodeCompanionChat Add<cr>",
+			{ noremap = true, silent = true, desc = "Add current selection to CodeCompanion Chat" }
+		)
 
-        -- Expand 'cc ' into 'CodeCompanion' in the command line
-        vim.cmd([[cab cc CodeCompanion]])
-    end,
+		-- Expand 'cc ' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
+	end,
 }
