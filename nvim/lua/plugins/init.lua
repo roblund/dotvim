134daaba2069ee92fd8a0443720e4aede1b54007
Rob Lund <rob.lund@onxmaps.com>
2025-10-13 16:17:29 -0600

    Remove some commented out code

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,36 +1,4 @@
 return {
-	-- { "JoosepAlviste/nvim-ts-context-commentstring" },
-	-- {
-	--     "numToStr/Comment.nvim",
-	--     dependencies = {
-	--         "JoosepAlviste/nvim-ts-context-commentstring",
-	--         "nvim-treesitter/nvim-treesitter",
-	--     },
-	--     config = function()
-	--         require("Comment").setup({
-	--             pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-	--             -- pre_hook = require('ts_context_commentstring.internal').calculate_commentstring,
-	--         })
-	--         require('nvim-ts-context-commentstring').setup({
-	--             enable_autocmd = true,
-	--         })
-	--     end,
-	-- },
-	-- {
-	--     'numToStr/Comment.nvim',
-	--     config = function()
-	--         require('Comment').setup {
-	--             pre_hook = function()
-	--                 return vim.bo.commentstring
-	--             end
-	--         }
-	--     end,
-	--     lazy = false,
-	--     dependencies = {
-	--         "JoosepAlviste/nvim-ts-context-commentstring",
-	--         'nvim-treesitter/nvim-treesitter',
-	--     }
-	-- },
 	{ "leafOfTree/vim-vue-plugin" },
 	{ "tpope/vim-unimpaired" },
 	{ "tpope/vim-surround" },
@@ -47,11 +15,7 @@ return {
 	},
 	{
 		"stevearc/oil.nvim",
-		---@module 'oil'
-		---@type oil.SetupOpts
 		opts = {},
-		-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 		lazy = false,
 		config = function()
 			require("oil").setup()
