cc5e1b85a397d74e1374b4c571908a10fb595491
Rob Lund <rob.lund@onxmaps.com>
2025-10-13 15:52:18 -0600

    lsp config file spacing

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,88 +1,89 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-        -- LSP Support
-        { "neovim/nvim-lspconfig" }, -- Required
-        {
-            -- Optional
-            "williamboman/mason.nvim",
-            build = function()
-                pcall(vim.cmd, "MasonUpdate")
-            end,
-        },
-        { "williamboman/mason-lspconfig.nvim" }, -- Optional
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
 
-        -- Autocompletion
-        { "hrsh7th/nvim-cmp" }, -- Required
-        { "hrsh7th/cmp-nvim-lsp" }, -- Required
-        { "L3MON4D3/LuaSnip" }, -- Required
-        { "hrsh7th/cmp-buffer" },
-        { "hrsh7th/cmp-nvim-lua" },
-        { "hrsh7th/cmp-nvim-lsp-signature-help" },
-    },
-    config = function()
-        local lsp = require("lsp-zero").preset({})
+	dependencies = {
+		-- LSP Support
+		{ "neovim/nvim-lspconfig" }, -- Required
+		{
+			-- Optional
+			"williamboman/mason.nvim",
+			build = function()
+				pcall(vim.cmd, "MasonUpdate")
+			end,
+		},
+		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-        lsp.on_attach(function(client, bufnr)
-            lsp.default_keymaps({ buffer = bufnr })
-        end)
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" }, -- Required
+		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+		{ "L3MON4D3/LuaSnip" }, -- Required
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-nvim-lua" },
+		{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+	},
+	config = function()
+		local lsp = require("lsp-zero").preset({})
 
-        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+		lsp.on_attach(function(client, bufnr)
+			lsp.default_keymaps({ buffer = bufnr })
+		end)
 
-        require("lspconfig").ts_ls.setup({
-            init_options = {
-                plugins = {
-                    {
-                        name = "@vue/typescript-plugin",
-                        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-                        languages = { "javascript", "typescript", "vue" },
-                    },
-                },
-            },
-            filetypes = {
-                "javascript",
-                "typescript",
-                "vue",
-            },
-        })
+		require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
-        lsp.setup()
+		require("lspconfig").ts_ls.setup({
+			init_options = {
+				plugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+						languages = { "javascript", "typescript", "vue" },
+					},
+				},
+			},
+			filetypes = {
+				"javascript",
+				"typescript",
+				"vue",
+			},
+		})
 
-        local cmp = require("cmp")
-        cmp.setup({
-            mapping = {
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            },
-            completion = {
-                keyword_length = 2,
-            },
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "nvim_lua" },
-                { name = "nvim_lsp_signature_help" },
-                { name = "luasnip" },
-                {
-                    name = "buffer",
-                    keyword_length = 2,
-                    max_item_count = 7,
-                    option = {
-                        -- allow completion of words in visible windows, not just current buffer
-                        get_bufnrs = function()
-                            local bufs = {}
-                            for _, win in ipairs(vim.api.nvim_list_wins()) do
-                                bufs[vim.api.nvim_win_get_buf(win)] = true
-                            end
-                            return vim.tbl_keys(bufs)
-                        end,
-                    },
-                },
-            },
-        })
+		lsp.setup()
 
-        vim.keymap.set("n", "<leader>v", function()
-            vim.cmd("vsplit")
-            vim.lsp.buf.definition()
-        end, { desc = "Goto definition in new vertical split" })
-    end,
+		local cmp = require("cmp")
+		cmp.setup({
+			mapping = {
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			},
+			completion = {
+				keyword_length = 2,
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "luasnip" },
+				{
+					name = "buffer",
+					keyword_length = 2,
+					max_item_count = 7,
+					option = {
+						-- allow completion of words in visible windows, not just current buffer
+						get_bufnrs = function()
+							local bufs = {}
+							for _, win in ipairs(vim.api.nvim_list_wins()) do
+								bufs[vim.api.nvim_win_get_buf(win)] = true
+							end
+							return vim.tbl_keys(bufs)
+						end,
+					},
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>v", function()
+			vim.cmd("vsplit")
+			vim.lsp.buf.definition()
+		end, { desc = "Goto definition in new vertical split" })
+	end,
 }
