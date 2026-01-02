c1196e3ca983d0985a4134feaaa39993216c8162
Rob Lund <rob.lund@onxmaps.com>
2026-01-02 14:46:34 -0700

    Drop verylazy from LSP loading

--- nvim/lua/rob/init.lua
+++ nvim/lua/rob/init.lua
@@ -2,11 +2,4 @@ require("rob.remap")
 require("rob.lazy")
 require("rob.set")
 require("rob.statusline")
-
--- Load LSP config after all plugins are loaded
-vim.api.nvim_create_autocmd("User", {
-	pattern = "VeryLazy",
-	callback = function()
-		require("rob.lsp")
-	end,
-})
+require("rob.lsp")
