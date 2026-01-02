d7b7cf83b6de475a956a617f13cac17dfeca1699
Rob Lund <rob.lund@onxmaps.com>
2026-01-02 15:16:58 -0700

    Comment out copilot plugin for now

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,17 +1,17 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({})
-	end,
-	opts = {
-        server_opts_overrides = {
-            settings = {
-                telemetry = {
-                    telemetryLevel = "off",
-                },
-            },
-		},
-	},
+	-- "zbirenbaum/copilot.lua",
+	-- cmd = "Copilot",
+	-- event = "InsertEnter",
+	-- config = function()
+	-- 	require("copilot").setup({})
+	-- end,
+	-- opts = {
+	--        server_opts_overrides = {
+	--            settings = {
+	--                telemetry = {
+	--                    telemetryLevel = "off",
+	--                },
+	--            },
+	-- 	},
+	-- },
 }
